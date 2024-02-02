173509bb2eba85b36251d8198d56a30a2c3ce511
EdenBlau <Eden4129@gmail.com>
2024-02-02 18:14:49 +0200

    changes

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const addBtn = document.getElementById('add');
-addBtn.addEventListener('click', () => addNewNote());
 
-function addNewNote(text = '') {
-    const note = document.getElementById('note');
-    note.style.display = 'block';
+addBtn.addEventListener('click', addNewNote);
+
+function addNewNote() {
+    const newNote = document.createElement('div');
+    newNote.classList.add('note');
+
+    newNote.innerHTML = `
+        <div class="note-buttons">
+            <button class="edit" alt="edit">
+                <img src="/images/edit-icon.jpg" width="25px" height="20px">
+            </button>
+            <button class="delete">
+                <img src="images/delete-icon.jpg" alt="delete" width="25px" height="20px">
+            </button>
+        </div>
+        <div class="main hidden"></div>
+        <textarea class="hidden"></textarea>
+    `
+
+    document.body.appendChild(newNote);
 }
-
-
-
