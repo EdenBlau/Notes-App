73487abc35f1225eb880a0418cb3d50e6b92fcd0
EdenBlau <Eden4129@gmail.com>
2024-02-14 17:41:08 +0200

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const addBtn = document.getElementById('add');
 
 addBtn.addEventListener('click', addNewNote);
 
-function addNewNote() {
+function addNewNote(text = '') {
     const newNote = document.createElement('div');
     newNote.classList.add('note');
 
@@ -11,13 +11,30 @@ function addNewNote() {
             <button class="edit" alt="edit">
                 <img src="/images/edit-icon.jpg" width="25px" height="20px">
             </button>
-            <button class="delete">
-                <img src="images/delete-icon.jpg" alt="delete" width="25px" height="20px">
+            <button class="delete" alt="delete>
+                <img src="/images/delete-icon.jpg" alt="delete" width="35px" height="20px">
             </button>
         </div>
-        <div class="main hidden"></div>
-        <textarea class="hidden"></textarea>
-    `
+        <div class="main ${text ? "" : "hidden"}></div>
+        <textarea class="${text ? "hidden" : ""}></textarea>
+        `;
 
     document.body.appendChild(newNote);
+
+    const editBtn = newNote.querySelector('.edit');
+    const deleteBtn = newNote.querySelector('.delete');
+    const main = newNote.querySelector('.main');
+    const textArea = newNote.querySelector('textarea');
+
+    deleteBtn.addEventListener('click', () => {
+        newNote.remove()
+    });
+
+    // editBtn.addEventListener('click', () => {
+    //     main.classList.toggle('hidden');
+    //     textArea.classList.toggle('hidden');
+    // });
+
+
+
 }
