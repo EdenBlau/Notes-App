ae0b7f76dd471f281118be166c8487d4a154f329
EdenBlau <Eden4129@gmail.com>
2024-02-21 18:02:34 +0200

    new changes

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const addBtn = document.getElementById('add');
+
+
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if (notes) {
+    notes.forEach(note => addNewNote(note))
+}
 addBtn.addEventListener('click', () => addNewNote());
 
 function addNewNote(text = '') {
@@ -29,9 +36,13 @@ function addNewNote(text = '') {
     const main = note.querySelector('.main');
     const textArea = note.querySelector('textarea');
 
+    textArea.value = text;
+    main.innerHTML = marked(text);
 
     deleteBtn.addEventListener('click', () => {
         note.remove();
+
+        updateLS();
     });
 
     editBtn.addEventListener('click', () => {
@@ -42,7 +53,16 @@ function addNewNote(text = '') {
     textArea.addEventListener('input', (e) => {
         const { value } = e.target;
         main.innerHTML = marked(value);
+
+        updateLS()
     });
 
     document.body.appendChild(note);
+}
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea');
+    const notes = [];
+    notesText.forEach(note => notes.push(note.value))
+    localStorage.setItem('notes', JSON.stringify(notes))
 }
